2f7a8c763c66cade5ca58fb2f953dfe6be2e276c
Lester <sonorous.lester@gmail.com>
2025-01-15 12:59:39 +0800

    Init project

---
+++ src/main/java/com/example/coursescheduler/CourseSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursescheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseSchedulerApplication.class, args);
+    }
+
+}
