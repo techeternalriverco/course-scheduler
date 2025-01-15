c5db654c5011623e9f290b0a56195258c832ca86
Lester <sonorous.lester@gmail.com>
2025-01-15 13:55:07 +0800

    Add Course entity

---
+++ src/main/java/com/example/coursescheduler/entity/Course.java
@@ -0,0 +1,48 @@
+package com.example.coursescheduler.entity;
+
+import jakarta.persistence.*;
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String time;
+
+    public Course() {}
+
+    public Course(String name, String time) {
+        this.name = name;
+        this.time = time;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
